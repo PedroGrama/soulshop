301798f4dcdcf94a4091dfd369f45b298e873b4e
silas-sousa <silassousadejesus@gmail.com>
2022-02-11 16:39:04 -0300

    correção

--- routes/cupomRoutes.js
+++ routes/cupomRoutes.js
@@ -6,7 +6,7 @@ router.get("/cupom", CupomController.paginaCupom);
 router.get("/cupom/novo", CupomController.paginaAdicionarCupom);
 router.get("/cupom/editar/:id",CupomController.paginaEditCupom);
 router.post("cupom/atualizar",CupomController.editCupom);
-router.post("cupom/enviar", CupomController.addCupom);
-router.post("cupom/deletar", CupomController.deleteCupom);
+router.post("/cupom/enviar", CupomController.addCupom);
+router.post("/cupom/deletar", CupomController.deleteCupom);
 
 module.exports = router;
--- routes/produtosRoutes.js
+++ routes/produtosRoutes.js
@@ -3,6 +3,9 @@ const ProdutoController = require("../controllers/ProdutoController");
 const router = Router();
 
 router.get("/produtos", ProdutoController.paginaProdutos);
+
+
+
 router.get("/produtos/novo", ProdutoController.paginaAdicionarProduto);
 router.get("/produtos/editar/:id", ProdutoController.paginaEditProduto);
 router.post("/produtos/atualizar", ProdutoController.editProduto);
